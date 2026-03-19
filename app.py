2bbb982d515c61e0a7b33171b58d12cafcf0d408
guanjie917 <guanjie@ucsb.edu>
2026-03-19 11:25:19 +0000

    1

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def main():
     return(render_template("main.html"))
 
+@app.route("/transferMoney",methods=["GET","POST"])
+def main():
+    return(render_template("transferMoney.html"))
+
 if __name__ == "__main__":
     app.run()
